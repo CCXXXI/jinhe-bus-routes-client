45f9b95177d1483075e0f2c53ee7a98b33759fb4
CCXXXI <ccxxxi@qq.com>
2021-10-31 16:40:00 +0800

    refactor: improve database init

--- jinhe/lib/utils/database.dart
+++ jinhe/lib/utils/database.dart
@@ -6,8 +6,11 @@ import 'package:path_provider/path_provider.dart';
 
 part 'database.g.dart';
 
-Future<void> initDatabase({bool clear = false}) async {
-  if (!GetPlatform.isWeb && GetPlatform.isDesktop) {
+Future<void> initDatabase({
+  bool clear = false,
+  bool forceDefaultInit = false,
+}) async {
+  if (!GetPlatform.isWeb && GetPlatform.isDesktop && !forceDefaultInit) {
     final dir = await getApplicationSupportDirectory();
     Hive.init(dir.path);
   } else {
