b760360bb62aa74ad78c227a08155f9952c205ed
CCXXXI <ccxxxi@qq.com>
2021-10-31 14:19:22 +0800

    feat(settings): update settings

--- jinhe/lib/settings/settings_logic.dart
+++ jinhe/lib/settings/settings_logic.dart
@@ -1,3 +1,86 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:quiver/iterables.dart';
 
-class SettingsLogic extends GetxController {}
+import '../utils/database.dart';
+import '../utils/log.dart';
+import '../utils/string.dart';
+import '../utils/web.dart';
+import 'theme.dart';
+
+class SettingsLogic extends GetxController with L {
+  void primaryOnChanged(Color v) {
+    theme
+      ..primary = v
+      ..save();
+    updateTheme();
+  }
+
+  void secondaryOnChanged(Color v) {
+    theme
+      ..secondary = v
+      ..save();
+    updateTheme();
+  }
+
+  void surfaceOnChanged(Color v) {
+    theme
+      ..surface = v
+      ..save();
+    updateTheme();
+  }
+
+  void backgroundOnChanged(Color v) {
+    theme
+      ..background = v
+      ..save();
+    updateTheme();
+  }
+
+  /// - null: loading
+  /// - empty: failed
+  final latestVer = Rx<String?>(null);
+
+  @override
+  void onInit() {
+    super.onInit();
+    updateVerInfo();
+  }
+
+  void updateVerInfo() async {
+    latestVer.value = null;
+    latestVer.value = await _getLatestVer() ?? '';
+    if (updateAvailable) {
+      Get.snackbar('发现新版本', '$version → ${latestVer.value}');
+    }
+  }
+
+  bool get updateAvailable {
+    final latest = latestVer.value;
+
+    if (latest == null || latest.isEmpty) return false;
+
+    final l = latest.split('.').map(int.parse);
+    final v = version.split('.').map(int.parse);
+    for (final pair in zip([l, v])) {
+      if (pair[0] > pair[1]) return true;
+      if (pair[0] < pair[1]) return false;
+    }
+
+    return false;
+  }
+
+  /// Get latest release version from GitHub.
+  Future<String?> _getLatestVer() async {
+    try {
+      final r = await dio.get(
+        Api.releases,
+        queryParameters: {'per_page': 1},
+      );
+      return (r.data[0]['name'] as String).substring(1);
+    } catch (e) {
+      l.error(e);
+      Get.snackbar('获取最新版本失败', e.toString());
+    }
+  }
+}
--- jinhe/lib/settings/settings_view.dart
+++ jinhe/lib/settings/settings_view.dart
@@ -1,6 +1,14 @@
+import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_settings_screens/flutter_settings_screens.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 
+import '../utils/database.dart';
+import '../utils/loading.dart';
+import '../utils/string.dart';
+import '../utils/web.dart';
 import 'settings_logic.dart';
 
 class SettingsPage extends StatelessWidget {
@@ -10,6 +18,104 @@ class SettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('设置'),
+      ),
+      body: Center(
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: 800),
+          child: ListView(
+            children: [
+              SettingsGroup(
+                title: '主题',
+                children: [
+                  ColorPickerSettingsTile(
+                    settingKey: 'theme.color.primary',
+                    title: '主题色',
+                    defaultValue: theme.primary,
+                    onChange: logic.primaryOnChanged,
+                  ),
+                  ColorPickerSettingsTile(
+                    settingKey: 'theme.color.secondary',
+                    title: '辅助色A'.s,
+                    defaultValue: theme.secondary,
+                    onChange: logic.secondaryOnChanged,
+                  ),
+                  ColorPickerSettingsTile(
+                    settingKey: 'theme.color.surface',
+                    title: '辅助色B'.s,
+                    defaultValue: theme.surface,
+                    onChange: logic.surfaceOnChanged,
+                  ),
+                  ColorPickerSettingsTile(
+                    settingKey: 'theme.color.background',
+                    title: '背景色',
+                    defaultValue: theme.background,
+                    onChange: logic.backgroundOnChanged,
+                  ),
+                ],
+              ),
+              SettingsGroup(
+                  title: '关于',
+                  children: ListTile.divideTiles(
+                    context: context,
+                    tiles: [
+                      ListTile(
+                        title: const Text('当前版本'),
+                        trailing: const Text(version),
+                        onTap: Url.version(version).launch,
+                      ),
+                      Obx(() => ListTile(
+                            title: const Text('最新版本'),
+                            trailing: logic.latestVer.value == null
+                                ? Loading()
+                                : logic.latestVer.value!.isEmpty
+                                    ? IconButton(
+                                        onPressed: logic.updateVerInfo,
+                                        icon:
+                                            const FaIcon(FontAwesomeIcons.redo),
+                                      )
+                                    : Badge(
+                                        child: Text(logic.latestVer.value!),
+                                        badgeContent: FaIcon(
+                                          logic.updateAvailable
+                                              ? FontAwesomeIcons.exclamation
+                                              : FontAwesomeIcons.check,
+                                          size: 16,
+                                        ),
+                                        badgeColor: logic.updateAvailable
+                                            ? Colors.orange
+                                            : Colors.green,
+                                        position: BadgePosition.topStart(
+                                            top: -4, start: -32),
+                                      ),
+                            onTap: Url.latest.launch,
+                          )),
+                      ListTile(
+                        title: const Text('反馈'),
+                        trailing: FaIcon(
+                          FontAwesomeIcons.github,
+                          color: theme.onBackground,
+                        ),
+                        onTap: Url.issues.launch,
+                      ),
+                      AboutListTile(
+                        applicationIcon: SvgPicture.asset(
+                          'assets/images/icon.svg',
+                          color: theme.onBackground,
+                          width: 42,
+                        ),
+                        applicationLegalese: license,
+                        applicationVersion: release,
+                        child: const Text('许可协议'),
+                      ),
+                    ],
+                  ).toList()),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
