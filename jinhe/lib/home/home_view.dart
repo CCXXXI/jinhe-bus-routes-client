b0451e304ca567b53e0fba2b0f4ebe19aae5c732
CCXXXI <ccxxxi@qq.com>
2021-10-31 13:03:08 +0800

    feat(home): basic home view

--- jinhe/lib/home/home_view.dart
+++ jinhe/lib/home/home_view.dart
@@ -1,6 +1,14 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 
+import '../settings/settings_view.dart';
+import '../utils/database.dart';
+import '../utils/string.dart';
+import '../utils/web.dart';
 import 'home_logic.dart';
 
 class HomePage extends StatelessWidget {
@@ -10,6 +18,31 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.all(e * pi),
+          child: SvgPicture.asset(
+            'assets/images/icon.svg',
+            color: theme.onPrimary,
+          ),
+        ),
+        title: const Text(appName),
+        actions: [
+          if (GetPlatform.isWeb)
+            Padding(
+              padding: const EdgeInsets.all(pi * e),
+              child: ElevatedButton(
+                onPressed: Url.latest.launch,
+                child: const Text('下载客户端'),
+              ),
+            ),
+          IconButton(
+            onPressed: () => Get.to(() => SettingsPage()),
+            icon: const FaIcon(FontAwesomeIcons.cog),
+          ),
+        ],
+      ),
+    );
   }
 }
