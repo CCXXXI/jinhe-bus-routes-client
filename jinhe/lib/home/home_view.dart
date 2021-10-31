126e4f71b4782eefdc9e0aa25e71b87bafc8d9d3
CCXXXI <ccxxxi@qq.com>
2021-10-31 18:41:00 +0800

    fix(home): no download for ios

--- jinhe/lib/home/home_view.dart
+++ jinhe/lib/home/home_view.dart
@@ -29,7 +29,7 @@ class HomePage extends StatelessWidget {
         ),
         title: const Text(appName),
         actions: [
-          if (GetPlatform.isWeb)
+          if (GetPlatform.isWeb && !GetPlatform.isIOS)
             Padding(
               padding: const EdgeInsets.all(pi * e),
               child: ElevatedButton(
