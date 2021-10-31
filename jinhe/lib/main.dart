2da6b107f29e2767f647d8d20f80a1f28a06eeed
CCXXXI <ccxxxi@qq.com>
2021-10-31 13:03:49 +0800

    feat: init everything

--- jinhe/lib/main.dart
+++ jinhe/lib/main.dart
@@ -1,115 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_settings_screens/flutter_settings_screens.dart';
+import 'package:get/get.dart';
+import 'package:loggy/loggy.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
+import 'package:window_size/window_size.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'home/home_view.dart';
+import 'settings/theme.dart';
+import 'utils/database.dart';
+import 'utils/log.dart';
+import 'utils/string.dart';
+
+void main() async {
+  await initDatabase();
+  Settings.init();
+  initLog();
+  if (GetPlatform.isDesktop && !GetPlatform.isWeb) initDesktop();
+  await initMessages();
+  await initSentry(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return GetMaterialApp(
+      navigatorObservers: [SentryNavigatorObserver()],
+      title: appName,
+      home: HomePage(),
+      theme: appTheme,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+void initDesktop() {
+  logDebug('initDesktop begin.');
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  logDebug('ensureInitialized');
+  WidgetsFlutterBinding.ensureInitialized();
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  logDebug('setWindowTitle');
+  setWindowTitle(appName);
 
-  final String title;
+  logDebug('setWindowMinSize');
+  setWindowMinSize(const Size(512, 512));
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  logInfo('initDesktop end.');
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+Future<void> initSentry(Widget app) async {
+  logInfo('release: $release');
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  Sentry.configureScope(
+    (scope) => scope.user = SentryUser(
+      ipAddress: '{{auto}}',
+    ),
+  );
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+  await SentryFlutter.init(
+    (options) => options
+      ..dsn =
+          'https://85129d39be5542c4b1a6391f906006c2@o996799.ingest.sentry.io/6042560'
+      ..tracesSampleRate = 1
+      ..sendDefaultPii = true
+      ..release = release,
+    appRunner: () => runApp(app),
+  );
 }
