4a0081ac247fd3a3036fe2e71354f9ea75c8498f
CCXXXI <ccxxxi@qq.com>
2021-11-18 10:53:39 +0800

    chore: 0.4.0+7
    BREAKING CHANGE: Package name updated.

--- jinhe_client/lib/utils/string.dart
+++ jinhe_client/lib/utils/string.dart
@@ -7,8 +7,8 @@ import 'pangu.dart';
 // record them manually
 const appName = '金河市公交线路查询客户端';
 const packageName = 'io.github.ccxxxi.jinhe_client';
-const version = '0.3.1';
-const buildNumber = '6';
+const version = '0.4.0';
+const buildNumber = '7';
 
 const release = '$packageName@$version+$buildNumber';
 
