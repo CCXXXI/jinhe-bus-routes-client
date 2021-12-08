f7a43283e7e70991f1eec6bfc2d5bd6c91e0609f
CCXXXI <ccxxxi@qq.com>
2021-12-08 20:31:20 +0800

    feat(stats): UC-11A

--- jinhe_client/lib/stats/special_station/special_station_logic.dart
+++ jinhe_client/lib/stats/special_station/special_station_logic.dart
@@ -1,3 +1,24 @@
 import 'package:get/get.dart';
 
-class SpecialStationLogic extends GetxController {}
+import '../../home/query/query_logic.dart';
+
+class SpecialStationLogic extends GetxController {
+  final logic = Get.find<QueryLogic>();
+  final res = ''.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    getRes();
+  }
+
+  void getRes() async {
+    var c0 = 0, c1 = 0, c2 = 0;
+    for (final s in logic.stationMap.values.map((e) => e.zh).toSet()) {
+      if (s.startsWith('地铁')) c0++;
+      if (s.endsWith('(始发站)')) c1++;
+      if (s.endsWith('(终点站)')) c2++;
+    }
+    res.value = '地铁站：$c0\n始发站：$c1\n终点站：$c2';
+  }
+}
