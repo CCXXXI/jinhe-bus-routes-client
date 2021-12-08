032db1226a06ae2f23f117e824bdfe3f4208dd15
CCXXXI <ccxxxi@qq.com>
2021-12-08 19:22:12 +0800

    feat(stats): 分析查询

--- jinhe_client/lib/stats/stats_view.dart
+++ jinhe_client/lib/stats/stats_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 
 import 'stats_logic.dart';
@@ -12,9 +13,52 @@ class StatsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('统计'),
+        title: const Text('分析查询'),
       ),
       body: const Placeholder(),
+    );
+  }
+}
+
+class ToolCard extends StatelessWidget {
+  const ToolCard(
+    this.leading,
+    this.title,
+    this.subtitle, {
+    Key? key,
+    this.onTap,
+    this.onLongPress,
+    this.enabled = true,
+  }) : super(key: key);
+
+  final IconData leading;
+  final String title;
+  final Widget subtitle;
+  final void Function()? onTap;
+  final void Function()? onLongPress;
+  final bool enabled;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: enabled ? onTap : null,
+      onLongPress: enabled ? onLongPress : null,
+      style: ElevatedButton.styleFrom(
+        primary: context.theme.colorScheme.background,
+        onPrimary: context.theme.colorScheme.onBackground,
+        shadowColor: context.theme.colorScheme.onBackground,
+      ),
+      child: Center(
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: 256),
+          child: ListTile(
+            leading: FaIcon(leading),
+            title: Text(title),
+            subtitle: subtitle,
+            mouseCursor: MouseCursor.uncontrolled,
+          ),
+        ),
+      ),
     );
   }
 }
