53f77db25fc300b5148ff011f3c10236501b9f84
CCXXXI <ccxxxi@qq.com>
2021-12-08 20:00:36 +0800

    feat(stats): UC-10

--- jinhe_client/lib/stats/station_route/station_route_logic.dart
+++ jinhe_client/lib/stats/station_route/station_route_logic.dart
@@ -1,3 +1,24 @@
 import 'package:get/get.dart';
 
-class StationRouteLogic extends GetxController {}
+import '../../home/query/query_logic.dart';
+import '../../utils/web.dart';
+
+class StationRouteLogic extends GetxController {
+  final logic = Get.find<QueryLogic>();
+  final res = ''.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    getRes();
+  }
+
+  void getRes() async {
+    final List<dynamic> r = (await dio.get(Api.stationsRoutes)).data;
+    res.value = r
+        .map((s) =>
+            '${logic.stationMap[s[0]]!.str}\n${s[1]}条\n' +
+            s[2].map((route) => Route.fromFullName(route).str).join('、'))
+        .join('\n\n');
+  }
+}
--- jinhe_client/lib/stats/station_route/station_route_view.dart
+++ jinhe_client/lib/stats/station_route/station_route_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../../utils/loading.dart';
 import 'station_route_logic.dart';
 
 class StationRoutePage extends StatelessWidget {
@@ -14,7 +15,18 @@ class StationRoutePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('统计停靠路线'),
       ),
-      body: const Placeholder(),
+      body: ListView(
+        children: [
+          Obx(
+            () => logic.res.isEmpty
+                ? Loading()
+                : Text(
+                    logic.res.value,
+                    textAlign: TextAlign.center,
+                  ),
+          ),
+        ],
+      ),
     );
   }
 }
