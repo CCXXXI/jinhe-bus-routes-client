09164203e208bd50e1cf4dfba602d9f861a13215
CCXXXI <ccxxxi@qq.com>
2021-12-08 20:44:22 +0800

    feat(stats): UC-12

--- jinhe_client/lib/stats/route_type/route_type_logic.dart
+++ jinhe_client/lib/stats/route_type/route_type_logic.dart
@@ -1,3 +1,20 @@
 import 'package:get/get.dart';
 
-class RouteTypeLogic extends GetxController {}
+import '../../utils/web.dart';
+
+class RouteTypeLogic extends GetxController {
+  final res = ''.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    getRes();
+  }
+
+  void getRes() async {
+    final Map<String, dynamic> r = (await dio.get(Api.routesTypes)).data;
+    var buffer = '';
+    r.forEach((key, value) => buffer += '$key: $value\n');
+    res.value = buffer;
+  }
+}
