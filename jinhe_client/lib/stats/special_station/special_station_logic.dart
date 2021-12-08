c33f5d8e352cceca4846bc1d388a25cccac68575
CCXXXI <ccxxxi@qq.com>
2021-12-08 20:34:18 +0800

    fix(stats): UC-11A 返回站点名

--- jinhe_client/lib/stats/special_station/special_station_logic.dart
+++ jinhe_client/lib/stats/special_station/special_station_logic.dart
@@ -13,12 +13,14 @@ class SpecialStationLogic extends GetxController {
   }
 
   void getRes() async {
-    var c0 = 0, c1 = 0, c2 = 0;
+    var c0 = <String>[], c1 = <String>[], c2 = <String>[];
     for (final s in logic.stationMap.values.map((e) => e.zh).toSet()) {
-      if (s.startsWith('地铁')) c0++;
-      if (s.endsWith('(始发站)')) c1++;
-      if (s.endsWith('(终点站)')) c2++;
+      if (s.startsWith('地铁')) c0.add(s);
+      if (s.endsWith('(始发站)')) c1.add(s);
+      if (s.endsWith('(终点站)')) c2.add(s);
     }
-    res.value = '地铁站：$c0\n始发站：$c1\n终点站：$c2';
+    res.value = '地铁站：${c0.length}\n${c0.join('、')}\n\n'
+        '始发站：${c1.length}\n${c1.join('、')}\n\n'
+        '终点站：${c2.length}\n${c2.join('、')}';
   }
 }
