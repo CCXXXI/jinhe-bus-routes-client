ee595791c2cee6036b755fce711b5de58e03ddb3
CCXXXI <ccxxxi@qq.com>
2021-12-08 09:00:23 +0800

    feat(query): cache stations and routes

--- jinhe_client/lib/home/query/query_logic.dart
+++ jinhe_client/lib/home/query/query_logic.dart
@@ -1,3 +1,97 @@
+import 'dart:collection';
+
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:get/get.dart';
 
-class QueryLogic extends GetxController {}
+import '../../utils/web.dart';
+
+part 'query_logic.freezed.dart';
+
+class QueryLogic extends GetxController {
+  final dataList = <Data>[];
+  final busy = true.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    initDataList();
+  }
+
+  void initDataList() async {
+    final stations = (await dio.get(Api.stations)).data;
+    final sgMap = <String, StationGroup>{};
+    for (final s in stations) {
+      dataList.add(Station(s['zh'], s['en'], s['id']));
+      if (sgMap.containsKey(s['zh'])) {
+        sgMap[s['zh']]!.ids.add(s['id']);
+      } else {
+        sgMap[s['zh']] =
+            StationGroup(s['zh'], s['en'], SplayTreeSet.of([s['id']]));
+      }
+    }
+    for (final sg in sgMap.values) {
+      if (sg.ids.length > 1) dataList.add(sg);
+    }
+
+    final routes = (await dio.get(Api.routes)).data;
+    routes.forEach((k, v) {
+      if (v == '0') {
+        dataList.add(Route(k, null));
+      } else {
+        dataList.add(Route(k, true));
+        dataList.add(Route(k, false));
+      }
+    });
+
+    busy.value = false;
+  }
+}
+
+abstract class Data {
+  String get str;
+}
+
+@freezed
+class Station with _$Station implements Data {
+  Station._();
+
+  factory Station(String zh, String en, String id) = _Station;
+
+  @override
+  String get str => '$zh $en $id';
+}
+
+@freezed
+class StationGroup with _$StationGroup implements Data {
+  StationGroup._();
+
+  factory StationGroup(String zh, String en, SplayTreeSet<String> ids) =
+      _StationGroup;
+
+  @override
+  String get str => '$zh $en ${ids.length}个站点';
+}
+
+@freezed
+class Route with _$Route implements Data {
+  Route._();
+
+  factory Route(String name, bool? up) = _Route;
+
+  @override
+  String get str {
+    var r = '$name路';
+    if (up != null) {
+      r += ' ${up! ? '上' : '下'}行';
+    }
+    return r;
+  }
+
+  String get fullName {
+    var r = name;
+    if (up != null) {
+      r += up! ? 'u' : 'd';
+    }
+    return r;
+  }
+}
